9f17b8f9a25407defc40bd8527e816eba45573b7
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2021-03-07 00:26:25 -0300

    feat: inserting MailProvider in CreateUserUseCase

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 import { ICreateUserRequestDTO } from './ICreateUserDTO'
 import { User } from '../../entities/User'
+import { IMailProvider } from '../../providers/IMailProvider'
 
 export class CreateUserUseCase {
   constructor(
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
   ) {}
 
   async execute(data: ICreateUserRequestDTO) {
@@ -17,5 +19,18 @@ export class CreateUserUseCase {
     const user = new User(data)
 
     await this.usersRepository.save(user)
+
+    await this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email
+      },
+      from: {
+        name: 'Equipe do Meu App',
+        email: 'equipe@meuapp.com'
+      },
+      subject: 'Seja bem-vindo a plataforma',
+      body: '<p>Voce ja pode fazer login em nossa plataforma</p>'
+    })
   }
 }
